d489639882bcb23bee82156f59ce332852d91aae
Matthieu DIMIER <dimier.matt@gmail.com>
2024-12-06 08:19:10 +0100

    catalog start

--- app/controllers/appController.js
+++ app/controllers/appController.js
@@ -4,8 +4,12 @@ import { router } from "../router.js";
 const appController = {
     async homepage(req, res) {
         res.render("homepage")
+    },
+
+    async catalog(req, res) {
+        res.render("catalog")
     }
-}
+};
 
 
 export { appController }
--- app/router.js
+++ app/router.js
@@ -4,6 +4,7 @@ import { appController } from './controllers/appController.js';
 const router = Router();
 
 router.get('/', appController.homepage);
+router.get('/catalog', appController.catalog);
 
 
 export { router };
