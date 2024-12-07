8bf4fa8c08eef4fab7d1ff977ff2e57c58f6f54a
Matthieu DIMIER <dimier.matt@gmail.com>
2024-12-08 00:10:54 +0100

    Dynamisation homepage and catalog ok

--- app/controllers/appController.js
+++ app/controllers/appController.js
@@ -1,13 +1,33 @@
 import { router } from "../router.js";
+import { Coffee } from '../models/index.js';
 
 
 const appController = {
     async homepage(req, res) {
-        res.render("homepage")
+
+        const coffees = await Coffee.findAll({
+            limit: 3,
+            order: [['id', 'DESC']],
+            include: ['origin', 'caracteristic']
+        })
+        // console.log(coffees);
+    
+        res.render("homepage", { coffees })
     },
 
     async catalog(req, res) {
-        res.render("catalog")
+
+        const coffees = await Coffee.findAll({
+            order: [['id', 'ASC']],
+            include: ['origin', 'caracteristic']
+        })
+
+        const allCaracteristics = coffees.map(coffee => coffee.caracteristic.carac);
+        const uniqueCarac = [...new Set(allCaracteristics)];
+
+        console.log(uniqueCarac);
+
+        res.render("catalog", { coffees, uniqueCarac })
     },
 
     async product(req, res) {
